a85bf2ca8fc671ab694824a797bb6ca5fc8c34b6
Jacinthe Chong <raghavjacinthe@gmail.com>
2023-06-02 17:18:34 -0400

    complete dndCharacter

---
+++ typescript/dnd-character/dnd-character.ts
@@ -0,0 +1,33 @@
+export class DnDCharacter {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  hitpoints: number;
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 - DnDCharacter.getModifierFor(this.constitution);
+  }
+  public static generateAbilityScore(): number {
+    const rollArray: number[] = [];
+    for (let i = 0; i < 3; i++) {
+      rollArray.push(Math.random() * 6 + 1);
+    }
+    rollArray.sort().shift();
+    return rollArray.reduce((acc, element) => {
+      return acc + element;
+    }, 0);
+  }
+
+  public static getModifierFor(abilityValue: number): number {
+    return Math.floor((abilityValue - 10) / 2);
+  }
+}
